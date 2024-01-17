b5996ccb109177121fa357602d50c6c99a87cdbc
Jonathan White <contact@jonathancwhite.com>
2024-01-17 16:13:46 -0600

    feat: added league page with data fetching via state

--- frontend/src/features/pickems/pages/UserLeagues.jsx
+++ frontend/src/features/pickems/pages/UserLeagues.jsx
@@ -1,29 +1,46 @@
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { getLeagueById } from "../slices/leagueSlice";
 import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import { getLeagueById } from "../slices/leagueSlice";
 
 const UserLeagues = () => {
-	// const { id } = useParams();
-	// const dispatch = useDispatch();
-	// const { userInfo } = useSelector((state) => state.auth);
-	// const navigate = useNavigate();
+	const { id } = useParams();
+	const dispatch = useDispatch();
+	const { userInfo } = useSelector((state) => state.auth);
+	const navigate = useNavigate();
+	const { league, isLoading, isError, message } = useSelector(
+		(state) => state.league,
+	);
 
 	const token = Cookies.get("jwt");
 
-	// useEffect(() => {
-	// 	if (!userInfo) {
-	// 		navigate("/login");
-	// 	} else {
-	// 		dispatch(getLeagueById({ id, token }));
-	// 	}
-	// }, [dispatch, id, token, userInfo, navigate]);
+	useEffect(() => {
+		if (!userInfo) {
+			navigate("/login"); // early return
+		}
+
+		try {
+			dispatch(getLeagueById({ id, token }));
+		} catch (error) {
+			console.log(error);
+		}
+	}, [dispatch, id, token, userInfo, navigate]);
 
 	return (
 		<>
-			<h1>League Page</h1>
+			{isLoading ? (
+				<div className='centeredContainer'>
+					<div className='spinner'></div>
+				</div>
+			) : (
+				<>
+					<h1>{league.name}</h1>
+					<p>{league.sport}</p>
+					<p>{league.tier}</p>
+				</>
+			)}
 		</>
 	);
 };
